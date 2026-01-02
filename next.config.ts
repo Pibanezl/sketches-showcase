7c9f19144c23553eddfedc97fb4682c0b5f1e780
Pibanezl <ped.ibanez@duocuc.cl>
2026-01-02 15:55:39 +1300

    first stage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// @type {import('next').NextConfig} 
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/sketches-showcase' : '',
+  assetPrefix: isProd ? '/sketches-showcase/' : '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
