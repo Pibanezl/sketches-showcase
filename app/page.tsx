80edeba868312b924830179c2c14a6ea53f32b5d
Pibanezl <ped.ibanez@duocuc.cl>
2026-01-05 02:43:40 +1300

    sketch1

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,12 @@ export default function Home() {
                 Sketch 1
               </button>
             </Link>
-        <Image
+        <img
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
-          priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
@@ -50,7 +49,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
+            <img
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
