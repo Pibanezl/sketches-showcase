019f9e1e37c634bcdb8b086c3a89d7c8da1a23f6
Pibanezl <ped.ibanez@duocuc.cl>
2026-01-05 02:53:29 +1300

    image

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import { prefix } from "./layout"; // para cargar imagenes xd
 
 
 
 export default function Home() {
-  const prefix =
-  process.env.NODE_ENV === "production"
-    ? "/sketches-showcase"
-    : "";
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
