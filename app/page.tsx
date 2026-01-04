fc66abd149ff37cb9f5aefc4ee0afe01f65cbfda
Pibanezl <ped.ibanez@duocuc.cl>
2026-01-05 02:48:04 +1300

    image

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import Link from "next/link";
 
 
 export default function Home() {
-
+  const prefix =
+  process.env.NODE_ENV === "production"
+    ? "/sketches-showcase"
+    : "";
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -13,12 +16,13 @@ export default function Home() {
                 Sketch 1
               </button>
             </Link>
-        <img
+        <Image
           className="dark:invert"
-          src="/next.svg"
+          src={`${prefix}/next.svg`}
           alt="Next.js logo"
           width={100}
           height={20}
+          priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
@@ -49,9 +53,9 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
+            <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src={`${prefix}/vercel.svg`}
               alt="Vercel logomark"
               width={16}
               height={16}
